eeea87b593d153d0b9977c6e29eb49dfca2d214c
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-05-24 22:11:31 +0300

    first php code

---
+++ ilk-uygulama.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- http://localhost/courseApp/ilk-uygulama.php -->
+     <?php echo phpinfo(); ?>
+    <?php
+        $title="PHP Dersleri";  
+        //değişken tanımlama
+    ?>
+    <h1>
+        <?php echo $title; ?>
+        <br>
+        <?php echo "1. ders"; ?>
+    </h1>
+    
+
+</body>
+</html>

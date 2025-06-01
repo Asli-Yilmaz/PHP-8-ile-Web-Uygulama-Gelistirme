0e276a74801734e55d126a27bb339d741224bc55
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-06-01 20:47:47 +0300

    sabitler

---
+++ sabitler.php
@@ -0,0 +1,9 @@
+<?php
+    //sabitler veri saklamak için kullanılır ancak tanımlanan değer değiştirilemez
+    define("baglanti","mysql bağlanti cümlesi");
+    const baglanti2="postgresql bağlanti cümlesi";
+    //sabitler yukarıdaki gibi iki şekilde tanımlanabilir
+    echo baglanti2;
+    echo "<br>";
+    echo baglanti;
+?>

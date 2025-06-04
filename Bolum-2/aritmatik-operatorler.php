69909772a3cd843051b4822039a758bb72ccca45
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-06-04 10:23:25 +0300

    aritmatik ve atam operatoleri

---
+++ Bolum-2/aritmatik-operatorler.php
@@ -0,0 +1,14 @@
+<?php
+    $a=10;
+    $b=20;
+    echo $a+$b."<br>";
+    echo $a-$b."<br>";
+    echo $a*$b."<br>";
+    echo $a/$b."<br>";
+    echo $a%$b."<br>"; //mod alma
+    echo $a**$b."<br>"; //üst alma
+    echo ++$a."<br>";//once arttır sonra yazdır
+    echo $b++."<br>";//once yazdır sonra arttır
+    echo $b."<br>";
+
+?>
---
+++ Bolum-2/atama-operatorleri.php
@@ -0,0 +1,11 @@
+<?php
+    $a;
+    $a=10;
+    $b=$a+20;
+    echo $a."<br>";
+    echo $b."<br>";
+    $b-=$a; //$b=$b-$a;
+    echo $b."<br>";
+    $a.=$b; //a ve b sayılarını string olarak birleştirir aynı işlem string birleştirmede de kullanılır
+    echo $a."<br>";
+?>

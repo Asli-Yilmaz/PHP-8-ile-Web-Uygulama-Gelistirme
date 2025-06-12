5b47c122f204041c44493d8574a197912de76694
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-06-12 12:00:31 +0300

    parametreler ve uygulamalar

--- Bolum-5/parametre-veri-tipleri.php
+++ Bolum-5/parametre-veri-tipleri.php
@@ -3,8 +3,9 @@
     //normalde çevirilebilen string tplerini sayısal veriye çevirerek
     //işlem yapabilmesine rağmen strict_types ile artık bu ilemi yapmaz
     //ve hata dönderir. boylece yazılan kodun güvenilirliği artar
-    function toplama(int $sayi1,int $sayi2){
+    function toplama(int $sayi1,int $sayi2):int{
         return $sayi1+$sayi2;
     }
+    //:int =int turunde değer dönder demek
     echo toplama("10","20");
 ?>

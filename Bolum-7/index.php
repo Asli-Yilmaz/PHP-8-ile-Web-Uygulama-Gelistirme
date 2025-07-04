3a2ad220cd33add085a2c7076aeb3d428ca9e11d
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-07-04 10:44:41 +0300

    partial view- dinamik veri kullanımı

---
+++ Bolum-7/index.php
@@ -0,0 +1,8 @@
+<?php $title="anasayfa"?>
+<?php include 'partials/_variables.php'?>
+<?php include 'partials/_header.php'?>
+    <main>
+        <h1>En Çok Satan Ürünler</h1>
+        <?php include 'partials/_urunler.php'?>
+    </main>
+<?php include 'partials/_footer.php'?>    

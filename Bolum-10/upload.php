b9c8c0bb6270be294c0b9134015fc2365c3a6abe
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-07-06 11:53:24 +0300

    changing destination of the uploaded file

--- Bolum-10/upload.php
+++ Bolum-10/upload.php
@@ -6,5 +6,16 @@
         print_r($_FILES["fileToUpload"]);
         print_r($_POST);
         echo "<pre>";
+
+        $dest_path="./uploadedFiles/";
+        $fileName=$_FILES["fileToUpload"]["name"];
+        $fileSourcePath=$_FILES["fileToUpload"]["tmp_name"];
+        $fileDestPath=$dest_path.$fileName;
+
+        if(move_uploaded_file($fileSourcePath,$fileDestPath)){
+            echo "dosya yüklendi";
+        }else{
+            echo "Hata";
+        }
     }
 ?>

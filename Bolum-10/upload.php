13fef5e9647780ff0e7b4cd97ff2004637dc178b
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-07-06 12:13:33 +0300

    selected file and size control

--- Bolum-10/upload.php
+++ Bolum-10/upload.php
@@ -2,20 +2,44 @@
     //iki tane buton varsa ve tıklanan butonlara göre işlem yapılacaksa
     //butona isim vermek ve aşağıdaki gibi buton ismini kontrol etmek daha kullanışlı
     if(isset($_POST["btnFileUpload"]) && $_POST["btnFileUpload"]=="Upload"){
-        echo "<pre>";
-        print_r($_FILES["fileToUpload"]);
-        print_r($_POST);
-        echo "<pre>";
 
+        // dosya yüklenirken kontrol edilecekler
+        // - dosya seçilmiş mi?
+        // - dosyanın boyutu
+        // - dosyanın isminin kontrolü
+        // - dosya uzantısı
+
+
+        // echo "<pre>";
+        // print_r($_FILES["fileToUpload"]);
+        // print_r($_POST);
+        // echo "<pre>";
+
+        $successfulUpload=true;
         $dest_path="./uploadedFiles/";
         $fileName=$_FILES["fileToUpload"]["name"];
+        $fileSize=$_FILES["fileToUpload"]["size"];
+
+        if(empty($fileName)){
+            $successfulUpload=false;
+            echo "Dosya Seçiniz."."<br>";
+        }
+
+        if($fileSize>50000){
+            $successfulUpload=false;
+            echo "Dosya Boyutu Fazla"."<br>";
+        }
+
         $fileSourcePath=$_FILES["fileToUpload"]["tmp_name"];
         $fileDestPath=$dest_path.$fileName;
+        if($successfulUpload){
 
-        if(move_uploaded_file($fileSourcePath,$fileDestPath)){
-            echo "dosya yüklendi";
-        }else{
-            echo "Hata";
+            if(move_uploaded_file($fileSourcePath,$fileDestPath)){
+            echo "dosya yüklendi"."<br>";
+            }else{
+                echo "Dosya Yüklenemedi"."<br>";
+            }
         }
+       
     }
 ?>

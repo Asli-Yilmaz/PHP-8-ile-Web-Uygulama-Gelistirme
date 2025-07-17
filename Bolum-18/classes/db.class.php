15f745005ee88d1fb819fbdfc689536669f6410a
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-07-17 22:35:05 +0300

    database class oluşturulması

---
+++ Bolum-18/classes/db.class.php
@@ -0,0 +1,25 @@
+<?php
+
+    class db{
+        private $host="localhost";
+        private $user="root";
+        private $password="";
+        private $dbName="mydb";
+
+        //daha sonradan db classından sınıflar türetilecek ve bu 
+        //türetilen yeni classların connect methoduna ulaşabilmesi için protected yapılmalı
+        protected function connect(){
+            try{
+                $dataSourceName="mysql:host=".$this->host.";dbname=".$this->dbName;
+                $pdo=new PDO($dataSourceName,$this->user,$this->password);
+                //hata gösterme modunun tanımlanması
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                $pdo->setAttribute(pdo::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+                
+                return $pdo;
+            }catch(PDOException $error){
+                echo "Bağlantu hatası: ".$error->getMessage()."<br>";
+            }
+        }
+    }
+?>
